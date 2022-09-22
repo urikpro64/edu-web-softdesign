2b77cc507d83b2173aa2c1363c98a7de8d735ee1
urikpro64 <urikpro64@gmail.com>
2022-09-23 05:01:47 +0700

    Test push to branch setting and merge to main

--- src/main/java/com/solfwaredesign/projectedu/ProjecteduApplication.java
+++ src/main/java/com/solfwaredesign/projectedu/ProjecteduApplication.java
@@ -11,7 +11,7 @@ public class ProjecteduApplication {
 
 	@RequestMapping("/")
 	String home(){
-		return "Hello";
+		return "HI";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(ProjecteduApplication.class, args);
