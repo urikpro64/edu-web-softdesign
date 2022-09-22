f7654b85ba8df278095f9eef1db9512cb2e18065
urikpro64 <urikpro64@gmail.com>
2022-09-22 16:55:03 +0700

    first commit

---
+++ src/main/java/com/solfwaredesign/projectedu/ProjecteduApplication.java
@@ -0,0 +1,13 @@
+package com.solfwaredesign.projectedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecteduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecteduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solfwaredesign/projectedu/ProjecteduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solfwaredesign.projectedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecteduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
