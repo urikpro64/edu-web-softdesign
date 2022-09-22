8eb9e964a69ee654ef4fdbf4d2b99967ded34cc1
urikpro64 <urikpro64@gmail.com>
2022-09-23 06:39:21 +0700

    ss

--- src/main/java/com/solfwaredesign/projectedu/ProjecteduApplication.java
+++ src/main/java/com/solfwaredesign/projectedu/ProjecteduApplication.java
@@ -11,7 +11,7 @@ public class ProjecteduApplication {
 
 	@RequestMapping("/")
 	String home(){
-		return "VVV";
+		return "XCXCXV";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(ProjecteduApplication.class, args);
