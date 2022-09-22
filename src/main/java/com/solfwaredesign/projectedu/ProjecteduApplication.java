84dd3393f80fc939904f142e4e6a9b015ff5cc1c
urikpro64 <urikpro64@gmail.com>
2022-09-23 05:20:18 +0700

    TEST 3

--- src/main/java/com/solfwaredesign/projectedu/ProjecteduApplication.java
+++ src/main/java/com/solfwaredesign/projectedu/ProjecteduApplication.java
@@ -11,7 +11,7 @@ public class ProjecteduApplication {
 
 	@RequestMapping("/")
 	String home(){
-		return "Test";
+		return "SSSS";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(ProjecteduApplication.class, args);
