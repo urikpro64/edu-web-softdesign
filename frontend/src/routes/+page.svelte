ed49d519e04be77e3f3b3659562256bc86a5488b
urikpro64 <urikpro64@gmail.com>
2022-10-06 04:21:17 +0700

    build: add tailwind css

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Hello and welcome to my site!</h1>
-<a href="/users/1">User 1</a>
+<div class="flex-col">
+    <div class="flex-none">
+        <div class="text-3xl underline">Hello</div>
+    </div>
+    <div class="flex-1 w-32">
+        <a href="/users/1">User 1</a>
+    </div>
+
+</div>
--- frontend/src/routes/users/[slug]/+page.ts
+++ frontend/src/routes/users/[slug]/+page.ts
@@ -1,5 +1,3 @@
-import { error } from '@sveltejs/kit';
-
 /** @type {import('./$types').PageLoad} */
 export function load(data:any) {
     console.log(data.data);
