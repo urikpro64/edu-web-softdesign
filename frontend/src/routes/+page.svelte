28b092ae3fb495566f8d88e4d121ca1ed5f3d7a4
urikpro64 <urikpro64@gmail.com>
2022-10-10 21:55:45 +0700

    design:layout homepage

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -12,43 +12,31 @@
 </script>
 
 
-<div class="flex ">
-    <img src={lg} width="100"vspace="0" hspace="50">
-    <div class="flex flex-row text-xl mt-11 text-stone-600" >
-        <div class=" text-stone-900 font-bold" >CuteShop</div>
-        <div class=" ml-10" >Shop</div>
-        <div class=" ml-8" >About</div>
-    </div>
-        <div class="flex flex-row text-xl mt-8" >
-            <a href="/register">
-                <div class="justify-end text-right"><RegButton on:click={handleClick}/></div>
-            </a>
-            <a href="/login">
-                <div class="ml-5"><LogButton on:click={handleClick}/></div>
-            </a>
+<div class="flex my-1 w-screen font-sans">
+    <div class="flex flex-row w-full items-center text-xl text-stone-400 font-semibold" >
+        <img class="m-3 object-cover w-12" src={ lg } alt="logo">
+        <div class="mx-4 text-stone-900 font-bold" >CuteShop</div>
+        <div class="mx-4" >Shop</div>
+        <div class="mx-4" >About</div>
+        <div class="flex w-full items-center justify-end">
+            <div class="mx-4"><a href="/register">Login</a></div>
+            <div class="mx-4 py-2 px-4 border-2 border-pink-300 hover:bg-pink-300 rounded-lg text-pink-300 hover:text-white hover:shadow-md"><a href="/login">Register</a></div>
         </div>
+    </div>
 </div>
 
-<div class="box-border h-96 w-full p-4 bg-stone-700  text-amber-50 ">
-    <div class="text-center text-7xl mt-8" >CuteShop</div>
-    <div class=" text-center text-xl mt-8" >sadwqfffagasdwaagasdwaagafagasdwasdwada</div>
-    <div class=" text-center text-xl " >sadwqfffagasdwaagasdwaagafagasdwasdwada</div>
-    <div class="text-center text-xl " >sadwqfffagasdwaagasdwaagafagasdwasdwada</div>
-    <div class="text-center mt-8 "><input type="text" placeholder="Search" size="70" ></div>
-    <div class="flex justify-center mt-8 text-xl">
+<div class="flex flex-col h-96 w-full p-4 bg-stone-700  text-amber-50 items-center">
+    <div class="text-7xl my-4">CuteShop</div>
+    <div class="text-xl mt-4">sadwqfffagasdwaagasdwaagafagasdwasdwada</div>
+    <div class="mt-8"><input class="w-96 h-10 px-5 rounded-full text-black" type="text" placeholder="Search..." size="70" ></div>
+    <div class="flex mt-8 text-xl">
         <div class=" " >Recent</div>
-        <div class="ml-8 border-solid rounded-md border-2 border-white" >XNXX</div>
-        <div class="ml-8 border-solid rounded-md border-2 border-white" >JavMost</div>
-        <div class="ml-8 border-solid rounded-md border-2 border-white" >Xhamter</div>
-        <div class="ml-8 border-solid rounded-md border-2 border-white" >Pornhub</div>
-        <div class="ml-8 border-solid rounded-md border-2 border-white" >Nhentai</div>
+        <div class="px-3 ml-4 border-solid rounded-2xl border border-white" >XNXX</div>
+        <div class="px-3 ml-4 border-solid rounded-2xl border border-white" >JavMost</div>
+        <div class="px-3 ml-4 border-solid rounded-2xl border border-white" >Xhamter</div>
+        <div class="px-3 ml-4 border-solid rounded-2xl border border-white" >Pornhub</div>
+        <div class="px-3 ml-4 border-solid rounded-2xl border border-white" >Nhentai</div>
     </div>
 </div>
 
 <div class=" text-xl mt-5 ml-10" >Reccommend</div>
-
-    
-    
-
-
-    
